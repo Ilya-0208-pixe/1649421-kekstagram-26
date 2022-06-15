3eb45b1f677b66467d9a53e14c44ef26d4e290a3
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-06-15 21:59:08 +0400

    добавляет функции

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 //имя_функции(проверяемая_строка, максимальная_длина);
 // Результат: true, если строка проходит по длине, и false — если не проходит
 
-let = stringToTest;
-let = maximumLength;
-let calculateMiles = function (stringToTest, maximumLength) {
-  if (maximumLength <= 140) {
-    calculateMiles = true;
+function checkString(stringToTest, maximumLength) {
+  let result;
+  if (stringToTest.length <= maximumLength) {
+    result = true;
   } else {
-    calculateMiles = false;
+    result = false;
   }
-}
-
-//имя_функции(от, до); // Результат: целое число из диапазона "от...до"
-
-let nameFunctions = function (fromNumber, upNumber) {
-  let result = numberFirst < numberSecond;
   return result;
 }
+checkString('qwerty', 100); //true
+
+//имя_функции(от, до); // Результат: целое число из диапазона "от...до"
+console.log('---1 ', Math.random());
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min === max) {
+    return +Math.abs(min);
+  }
+  if (max > min) {
+    return +Math.abs(Math.floor(Math.random() * (max - min + 1) + min));
+  } else {
+    return +Math.abs(Math.floor(Math.random() * (min - max + 1) + max));
+  }
+}
+console.log('1, 10  == ', getRandom(1, 10));
+console.log('20, 30  == ', getRandom(20, 30));
+console.log('30, 20  == ', getRandom(30, 20));
+console.log('-10, 5  == ', getRandom(-10, 5));
+console.log('-10, -10  == ', getRandom(-10, -10));
+console.log('20, 20  == ', getRandom(20, 20));
